1cf23c55ef2c2d187af264a560ac5e934586468a
Seyed Faridoddin Kiaei <sfdkiaei@gmail.com>
2022-08-26 21:03:01 +0430

    feat: add loading data to dataframe :sparkles:

--- loader.py
+++ loader.py
@@ -1,6 +1,8 @@
 import os
 import patoolib
 import glob
+import pandas as pd
+from tqdm import tqdm
 
 class Loader:
     def __init__(self) -> None:
@@ -14,9 +16,24 @@ class Loader:
                 os.makedirs(outputpath)
                 patoolib.extract_archive(file, outdir=outputpath)
                 
-    def main(self):
-        pass
+    def create_df_from_xlsx(self):
+        files = glob.glob('data/*/*/‫حجم تردد ساعتی‬/*.xlsx')
+        df = pd.DataFrame()
+        for f in tqdm(files):
+            data = pd.read_excel(f, 'Sheet1', header=1)
+            df = pd.concat([df, data])
+        return df
+    
+    def save_df(self, df: pd.DataFrame):
+        df.to_pickle('data/data.pkl')
+        
+    def load_df(self) -> pd.DataFrame:
+        return pd.read_pickle('data/data.pkl')
+        
+        
         
 if __name__ == '__main__':
     loader = Loader()
-    loader.main()
+    df = loader.create_df_from_xlsx()
+    loader.save_df(df)
+    print(loader.load_df().shape)
